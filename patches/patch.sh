ba097b3b67d5f1fe4006740a5b73ecf9a43af881
Lionel <lionel.truflandier@gmail.com>
2026-02-24 16:19:39 +0100

    added patches for missing comma between descriptors...

--- patches/patch.sh
+++ patches/patch.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# how to patch
+# diff -u file file_corr >> file.patch
+# patch [options] file_to_patch < file.patch
+# [options] -b keep backtrack of original file
+# [options] -R reverse patching
+
 QEROOT=/Users/lioneltruflandier/q-e
+QEGIPAWROOT=/Users/lioneltruflandier/q-e-gipaw
 LD1ROOT=$QEROOT/atomic
 
-patch $LD1ROOT/src/ld1_readin.f90 < ld1_gipaw.patch 
+#patch -b $LD1ROOT/src/ld1_readin.f90 < ld1_gipaw.patch 
+#patch -b $QEGIPAWROOT/src/gipaw_routines.f90 < ms_gipaw_routines.patch
+#patch -b $QEGIPAWROOT/src/suscept_crystal.f90< ms_.suscept_crystal.patch
+
